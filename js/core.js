f2a75d409300e1b2c893ae1cd8878e4df774bc5b
YusselPP <yussel_paredes@hotmail.com>
2015-09-09 15:14:36 -0500

    Unir item.js y core.js
    Para reducir las peticiones http.

--- js/core.js
+++ js/core.js
@@ -114,3 +114,13 @@ function ajaxRequest(method, url, handle, data){
 	xmlhttp.open( method, url, true);
 	xmlhttp.send(data);
 }
+
+var ItemType = {};
+
+Object.defineProperty(ItemType, 'NORMAL', { value: 4 });
+
+Object.defineProperty(ItemType, 'SERIE', { value: 5 });
+
+Object.defineProperty(ItemType, 'LOTE', { value: 6 });
+
+Object.defineProperty(ItemType, 'SERVICE', { value: 7 });
