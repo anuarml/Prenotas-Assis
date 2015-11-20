6b2f43f8d6ca10d10dc7d1d59d290b59b46b5470
YusselPP <yussel_paredes@hotmail.com>
2015-11-20 13:24:00 -0600

    Corregido error al obtener la lista de impresoras
    Al intentar obtener la lista de impresoras el servidor devolvía un error
    500. Se cambió la función in_array por la función array_intersect para
    determinar la lista de impresoras a mostrar.

--- php/get_printer_list.php
+++ php/get_printer_list.php
@@ -1,19 +1,21 @@
 <?php
 require('../config/config.php');
-$printers = array();
+
+$avail_printers = array();
+$installed_printer_names = array();
 if (function_exists('printer_list')) {
     
-    $printerList = printer_list(PRINTER_ENUM_LOCAL,"",5);
-    foreach($printerList as $printer){
-	//echo $printer['PRINTERNAME'].'<br>';
-    	$printerName = $printer['PRINTERNAME'];
+    $installed_printers = printer_list(PRINTER_ENUM_LOCAL,"",5);
 
-	json_encode($cfg_printers);
-    	if(in_array($printerName,$cfg_printers))
-        	$printers[] = array('name' => $printer['PRINTERNAME'],'ip'=>'');
+    foreach($installed_printers as $installed_printer){
+	$installed_printer_names[] = $installed_printer['PRINTERNAME'];
     }
 
-    
+    $avail_printer_names = array_intersect($installed_printer_names,$cfg_printers);
+
+    foreach($avail_printer_names as $avail_printer_name){
+        $avail_printers[] = array('name' => $avail_printer_name,'ip'=>'');
+    }
 }
-echo json_encode($printers);
+echo json_encode($avail_printers);
 ?>
